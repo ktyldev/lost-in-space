243c2ec4ac36fad8de61e35b4c780e119b2e9200
Cat Flynn <mewtwo97@hotmail.com>
2017-11-24 20:18:02 +0000

    Add player movement

---
+++ Assets/General/GameTags.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTags
+{
+    public const string Horizontal = "Horizontal";
+    public const string Vertical = "Vertical";
+
+}
---
+++ Assets/Player/Ship.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveDirection = new Vector3(
+            Input.GetAxis(GameTags.Horizontal),
+            0,
+            Input.GetAxis(GameTags.Vertical)).normalized;
+        
+        transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
+        transform.LookAt(transform.position + moveDirection);
+    }
+
+    private Vector3 GetMoveDirection()
+    {
+        return new Vector3(
+            Input.GetAxis(GameTags.Horizontal), 
+            0, 
+            Input.GetAxis(GameTags.Vertical)).normalized;
+    }
+}
