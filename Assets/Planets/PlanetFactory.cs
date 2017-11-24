b273680d11f087d04ead5147c0f7860b979f7a5c
Cat Flynn <mewtwo97@hotmail.com>
2017-11-24 21:21:02 +0000

    Detect player leaving sector

---
+++ Assets/Planets/PlanetFactory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetFactory : MonoBehaviour
+{
+    public GameObject sector;
+
+    private List<Sector> _sectors;
+
+    void Awake()
+    {
+        _sectors = new List<Sector>();    
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        // Generate starting sector
+        _sectors.Add(GenerateSector(Vector3.zero));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator GenerateSectors()
+    {
+        print("Polling sector generation");
+        yield return new WaitForSeconds(1);
+    }
+
+    private Sector GenerateSector(Vector3 position)
+    {
+        var newSector = Instantiate(sector, transform).GetComponent<Sector>();
+        newSector.transform.position = position;
+
+        return newSector;
+    }
+}
---
+++ Assets/Planets/Sector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sector : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        var ship = other.gameObject.GetComponent<Ship>();
+        if (ship == null)
+            return;
+
+        print("ship leaving sector!");
+    }
+}
