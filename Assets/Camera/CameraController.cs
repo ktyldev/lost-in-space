7bfdfc4e5378ce4bd4c41d68f111269e65a78478
Cat Flynn <mewtwo97@hotmail.com>
2017-11-24 20:23:39 +0000

    Add basic camera

---
+++ Assets/Camera/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 _offset;
+    private Transform _tracked;
+    
+	// Use this for initialization
+	void Start () {
+        _tracked = GameObject.FindGameObjectWithTag(GameTags.Player).transform;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = _tracked.position + _offset;
+        transform.LookAt(_tracked);
+	}
+}
--- Assets/General/GameTags.cs
+++ Assets/General/GameTags.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class GameTags
 {
+    public const string Player = "Player";
+
+    // Input axes
     public const string Horizontal = "Horizontal";
     public const string Vertical = "Vertical";
-
 }
